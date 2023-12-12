0b99cab1e5e8dcb201f0a033eb311a32a4d5bc52
vijaykanwal90 <vijaykanwal90@gmail.com>
2023-12-12 19:39:24 +0530

    check wirking

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // console.log("hello world")
 const express = require('express')
 const app = express()
-const PORT = 3000
+const port = 3000
 app.get('/', (req, res)=>{
     res.send("Hello world!")
 })
 app.get('/login', (req, res)=>{
     res.send("welcome to login page")
 })
-app.listen(PORT , ()=>{
-console.log("Server listening on port ${PORT}")
+app.listen(port , ()=>{
+console.log(`Server listening on port ${port}`)
 })
