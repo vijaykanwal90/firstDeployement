d2aa428ff23732329ce239874f2a0d1a00100156
vijaykanwal90 <vijaykanwal90@gmail.com>
2023-12-13 20:26:47 +0530

    gdlgh

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // console.log("hello world")
 const express = require('express')
 const app = express()
-const port = 3000
+require('dotenv').config();
+
+// const port = 3000
 app.get('/', (req, res)=>{
     res.send("Hello world!")
 })
 app.get('/login', (req, res)=>{
     res.send("welcome to login page")
 })
-app.listen(port , ()=>{
-console.log(`Server listening on port ${port}`)
+app.listen(process.env.PORT , ()=>{
+console.log(`Server listening on port ${process.env.PORT}`)
 })
